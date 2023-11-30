2a0208cbdb76f8987cb8e0d18ce9fe48e2941736
LilyCrow <118573194+LilyCrow@users.noreply.github.com>
2023-11-30 08:34:28 -0800

    Switched DoughPuzzle logic to OnCollisionEnter/Exit

--- Assets/DoughPuzzle.cs
+++ Assets/DoughPuzzle.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 
 public class DoughPuzzle : MonoBehaviour
 {
-    public GameObject doughBag;
+    //public GameObject doughBag;
     public GameObject placemat;
     public bool doughPlacement;
         
     // Start is called before the first frame update
     void Start()
     {
-        
+        doughPlacement = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (placemat.transform.position == transform.position)
+        /*if (placemat.transform.position == transform.position)
         {
             doughPlacement = true;
         }
@@ -27,8 +27,26 @@ public class DoughPuzzle : MonoBehaviour
             doughPlacement = false;
         }
 
-        print(doughPlacement);
+        print(doughPlacement);*/
     }
-    
-    
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Placemat"))
+        {
+            doughPlacement = true;
+        }
+        else
+        {
+            doughPlacement = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Placemat"))
+        {
+            doughPlacement = false;
+        }
+    }
 }
