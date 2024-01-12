74c7ddf484163ffbf4c6295a07e01015bb17bb19
virtuosovodka <67210448+virtuosovodka@users.noreply.github.com>
2024-01-12 09:17:46 -0800

    adding timer and group box colliders
    the timer doesn't work so work on that, it is for letting a box freeze position on collision
    
    add box colliders on all shelves and crates

--- Assets/Lily/DoughPuzzle.cs
+++ Assets/Lily/DoughPuzzle.cs
@@ -9,6 +9,7 @@ public class DoughPuzzle : MonoBehaviour
    public GameManager gm;
    public GameObject placemat;
    public TextMeshProUGUI text;
+   public float timer = 0.0f;
         
     // Start is called before the first frame update
     void Start()
@@ -30,10 +31,16 @@ public class DoughPuzzle : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
+        timer += Time.deltaTime;
+        if (timer > .1)
+        {
+            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
         //if the object the dough bag collided with is the placemat, add 1 to doughPlacement
         if (other.gameObject == placemat)
         {
             gm.doughPlacement ++;
+            this.GetComponent<Transform>().rotation = Quaternion.identity;
             this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         }
         //if the dough bag collided with any other object keep doughPlacement at its' current value
@@ -46,6 +53,8 @@ public class DoughPuzzle : MonoBehaviour
 
     private void OnCollisionExit(Collision other)
     {
+        this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        timer = 0.0f;
         //if the dough bag is removed from/ is no longer touching the placemat, subtract 1 from doughPlacement
         if (other.gameObject == placemat)
         {
