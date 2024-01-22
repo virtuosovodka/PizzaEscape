c142248e83532b4bc83c4bde6a881b090ae3e383
LilyCrow <118573194+LilyCrow@users.noreply.github.com>
2024-01-22 09:14:13 -0800

    Light switch done

--- Assets/Lily/Light.cs
+++ Assets/Lily/Light.cs
@@ -7,6 +7,7 @@ public class Light : MonoBehaviour
 {
     public GameObject Player;
     public GameObject light;
+    public GameObject scaredText;
 
     public bool lightOn;
     public float buttonCoolDownTimer = 0;
@@ -15,22 +16,30 @@ public class Light : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //On start turn the light on and the "Pizza monster is scared of the dark" text off
         light.SetActive(true);
+        scaredText.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //The button cool down timer makes it so the button will only be "activated" once when touched
         buttonCoolDownTimer += Time.deltaTime;
     }
 
     private void OnCollisionEnter(Collision other)
     {
+        //If the object touching the light switch (object this script is attached to) is the player and the button
+        //cool down timer is greater than 0.5 frames, set the button cool down timer to zero, change the value of 
+        //lightOn. Then change whether or not the light is on based on the value of lightOn, make scaredText.SetActive
+        //the opposite of lightOn.
         if (other.gameObject == Player && buttonCoolDownTimer > .5f)
         {
             buttonCoolDownTimer = 0;
             lightOn = !lightOn;
             light.SetActive(lightOn);
+            scaredText.SetActive(!lightOn);
         }
     }
 }
