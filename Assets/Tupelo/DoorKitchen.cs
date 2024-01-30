31cb926b4fb1f1091c64105ac9bd41f279b2ea7d
tupelowd <109623361+tupelowd@users.noreply.github.com>
2024-01-30 10:00:26 -0800

    things are working with the key code

--- Assets/Tupelo/DoorKitchen.cs
+++ Assets/Tupelo/DoorKitchen.cs
@@ -12,29 +12,34 @@ public class DoorKitchen : MonoBehaviour
 
     [SerializeField]private TextMeshProUGUI codeText;
     string codeTextValue = "";
+    int codeTextValueInt;
     public string doorCode;
     public GameObject codePanel;
+    int doorCodeInt;
+    int tolerance = 8;
 
     // Start is called before the first frame update
     void Start()
     {
-     
+        clockScript = GameObject.FindObjectOfType<ClockScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
         codeText.text = codeTextValue;
+        //codeTextValueInt = int.Parse(codeTextValue.Trim());
+        //doorCodeInt = int.Parse(doorCode.Trim());
 
-        if (codeTextValue == doorCode)
+        //print(int.Parse(clockScript.theCode)+int.Parse(codeTextValue));
+        //print(codeTextValue);
 
+        if (((int.Parse(clockScript.theCode)+tolerance) >= int.Parse(codeTextValue)&& (int.Parse(clockScript.theCode) - tolerance)<= int.Parse(codeTextValue))) {
 
             //codeTextValue >= clockScript.theCode.int.Parse -8 && codeTextValue <= clockScript.theCode ++ 8
             //DO THIS NEXT CLASS CONVERT TO INTEGER
             //also figure out the issue with templayer chasing bool
-
-
-        {
+            //its working !!!!
             openDoor = true;
         }
 
