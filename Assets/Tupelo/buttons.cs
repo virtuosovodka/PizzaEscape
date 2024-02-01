2540ceb64857c440d9f2be9eb48e2b9c34b791d6
tupelowd <109623361+tupelowd@users.noreply.github.com>
2024-02-01 10:03:07 -0800

    pizza

---
+++ Assets/Tupelo/buttons.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttons : MonoBehaviour
+{
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+    public GameObject button5;
+    public GameObject button6;
+    public GameObject button7;
+    public GameObject button8;
+    public GameObject button9;
+    public GameObject button0;
+
+    public DoorKitchen doorKitchen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorKitchen = GameObject.FindObjectOfType<DoorKitchen>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "button1")
+        {
+            doorKitchen.codeTextValue = "2";
+        }
+    }
+}
