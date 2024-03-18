afb3171e9770342e9508546cacad97fb7c8147d4
tupelowd <109623361+tupelowd@users.noreply.github.com>
2024-03-18 10:00:49 -0700

    working on puzzle

--- Assets/Tupelo/PizzaMaking.cs
+++ Assets/Tupelo/PizzaMaking.cs
@@ -14,18 +14,30 @@ public class PizzaMaking : MonoBehaviour
     int hamNumber = 0;
     Vector3 endPosition;
     Vector3 startPosition;
-    float printerSpeed = 1f;
+    float printerSpeed = .02f;
+    private float startTime;
+    private float journeyLength;
+    //lerp
+  
+
+
     // Start is called before the first frame update
     void Start()
     {
         menuPiece2.transform.position = startPosition;
         endPosition = new Vector3(18.62f, 2.66f, 3.97f);
-        startPosition = new Vector3(18.62f, 2.47f, 3.75f);
+        startPosition = new Vector3(18.62f, 2.42f, 3.63f);
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startPosition, endPosition);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+
+
      if(pepperoniNumber == 5)
         {
             pizza1Done = true;
@@ -40,7 +52,10 @@ public class PizzaMaking : MonoBehaviour
         }
      if(pizza1Done && pizza2Done && pizza3Done)
         {
-            menuPiece2.transform.position = Vector3.MoveTowards(startPosition, endPosition, printerSpeed * Time.deltaTime);
+            float distCovered = (Time.time - startTime) * printerSpeed;
+            float fractionOfJourney = distCovered / journeyLength;
+            menuPiece2.transform.position = Vector3.Lerp(startPosition, endPosition, fractionOfJourney);
+            //menuPiece2.transform.position = Vector3.MoveTowards(startPosition, endPosition, printerSpeed * Time.deltaTime);
         }
     }
     private void OnCollisionEnter(Collision collision)
